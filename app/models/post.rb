3df294d373be58636e6621b1cdd6212ae586c79a
joaodouglasdantas02b <joaodouglasdantas02b@gmail.com>
2026-05-10 20:42:00 -0300

    feat: adicionando regras no model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 5 }
+
+  before_save :format_title
+
+  after_create :send_notification
+
+  def short_title
+    title.truncate(10)
+  end
+
+  def short_description
+    description.truncate(10)
+  end
+
+  def send_notification
+    puts "Post criado!"
+  end
+
+  private
+
+  def format_title
+    self.title = title.capitalize
+  end
 end
